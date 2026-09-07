b91f47fecf7b98537121047b5241e5fc7485a062
Peter Njoroge <njugunahpeternjoroge@gmail.com>
2026-09-07 10:36:49 +0300

    Add project files.

---
+++ productsManangement/Controllers/productsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using productsManangement.Models;
+
+namespace productsManangement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class productsController : ControllerBase
+    {
+
+        static List<Product>  products = new List<Product>
+            {
+                new Product { Id = 1, Name = "Product 1", Description = "Description for Product 1", Price = 10.99M },
+                new Product { Id = 2, Name = "Product 2", Description = "Description for Product 2", Price = 19.99M },
+                new Product { Id = 3, Name = "Product 3", Description = "Description for Product 3", Price = 5.99M }
+    };
+    [HttpGet]
+
+        public IActionResult GetProducts()
+        {
+            // Sample data for demonstration purposes
+            
+            return Ok(products);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            var response = products.FirstOrDefault(p => p.Id == id);
+            if (response == null)
+            {
+                return NotFound();
+            }
+            return Ok(response);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult UpdateProduct(int id, Product product)
+        {
+            var existingProduct = products.FirstOrDefault(p => p.Id == id);
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+            existingProduct.Name = product.Name;
+            existingProduct.Description = product.Description;
+            existingProduct.Price = product.Price;
+            return NoContent();
+        }
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var productToDelete = products.FirstOrDefault(p => p.Id == id);
+            if (productToDelete == null)
+            {
+                return NotFound();
+            }
+            products.Remove(productToDelete);
+            return NoContent();
+        }
+    }
+}
---
+++ productsManangement/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace productsManangement.Models
+{
+    public class Product
+    {
+
+        public  required int Id { get; set; }
+        public required string Name { get; set; }
+
+        public  string? Description { get; set; }
+
+
+        public  required decimal Price { get; set; }
+
+
+    }
+}
